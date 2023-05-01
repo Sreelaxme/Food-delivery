8e403d0f587e4873adb3723ee16ea3d66b5ccb16
Sreelakshmi <112001042@smail.iitpkd.ac.in>
2023-05-01 17:16:29 +0530

    changed few links

--- newpy.py
+++ newpy.py
@@ -2,6 +2,7 @@ from flask import Flask, request, session, redirect, url_for, render_template, f
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
 import re 
+from PIL import Image 
  
 app = Flask(__name__)
 app.secret_key = 'cairocoders-ednalan'
@@ -12,22 +13,20 @@ DB_USER = "proj_admin"
 DB_PASS = "password"
  
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
-def get_restaurants(name=None, cuisine=None, rating=None):
+def get_restaurants(name):
     #conn = sqlite3.connect('restaurant.db')
     c = conn.cursor()
 
-    query = "SELECT * FROM restaurant WHERE 1=1"
+    query = "select r.r_name,r.r_image_link,d.dish_image_link from restaurants as r join Restaurant_Items as i on i.r_id=r.r_id join Restaurant_Dish as d on d.dish_id=i.dish_id "
     if name:
-        query += f" AND name = '{name}'"
-    if cuisine:
-        query += f" AND cuisine = '{cuisine}'"
-    if rating:
-        query += f" AND rating = '{rating}'"
+        query += f" where r_name = '{name}'"
+    
 
     c.execute(query)
     restaurants = c.fetchall()
     conn.close()
-    
+    for i in restaurants:
+        print (i)
     return restaurants
 def top_5dishes(id):
     c=conn.cursor()
@@ -39,7 +38,11 @@ def top_5dishes(id):
     sorted_list = sorted(dishes, key=lambda x: x[1], reverse=True)[:5]
     # sort(dishes)
     for i in sorted_list:
-        print(i) 
+        # Open the image file
+        image = Image.open(i[2])
+        image.show()
+ 
     conn.close()
 
-top_5dishes(1)
+# top_5dishes(2)
+get_restaurants('Saffron Spice')
