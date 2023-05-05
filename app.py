ae3e729f61fb96884d9a44e4e0cf5cb188e7bf3b
Sreelakshmi <112001042@smail.iitpkd.ac.in>
2023-05-05 22:13:46 +0530

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, session, redirect, url_for, render_template, flash
 import psycopg2 #pip install psycopg2 
 import psycopg2.extras
@@ -5,82 +6,78 @@ import re
 from PIL import Image 
  
 app = Flask(__name__)
-app.secret_key = 'cairocoders-ednalan'
+
  
 DB_HOST = "localhost"
 DB_NAME = "admin"
 DB_USER = "proj_admin"
 DB_PASS = "password"
- 
-conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
 @app.route('/')
-def home():
-  
-    return render_template('index.html')
-@app.route('/', methods=['GET','POST'])
-def get_restaurants():
-    #conn = sqlite3.connect('restaurant.db')
+def top_5_restaurants():
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
     c = conn.cursor()
-
-    query = "select r.r_name,r.r_image_link,d.dish_image_link from restaurants as r join Restaurant_Items as i on i.r_id=r.r_id join Restaurant_Dish as d on d.dish_id=i.dish_id "
-    
-    query += f" where r_name = 'Saffron Spice'"
-    
-
+    query = "SELECT * FROM Restaurants ORDER BY rating DESC LIMIT 5"
     c.execute(query)
     restaurants = c.fetchall()
-    conn.close()
-    for i in restaurants:
-        print (i)
-    return render_template('rest.html',restaurant=restaurants)
-# with app.app_context():
-#     get_restaurants('Saffron Spice')
-@app.route('/dishes', methods=['GET','POST'])
-def top_5dishes(name,veg = None, cuisine = None ):
-    c=conn.cursor()
-    query1= "SELECT r_id from restaurants"
-    query1 += f" where r_name = '{name}'"
-    c.execute(query1)
-    id = c.fetchone()
-    # print(type(id[0]))
+    # conn.close()
+    return render_template('index.html', restaurants=restaurants)
 
-    query="SELECT d.dish_name, i.rating,d.dish_image_link FROM Restaurant_Dish AS d JOIN Restaurant_Items AS i ON d.dish_id = i.dish_id "
-    query+=f" where i.r_id='{id[0]}'"
-    if veg:
-        query+=f" and d.vegetarian='t'"
-    if cuisine:
-        query+=f" and d.cousine ='{cuisine}'"
+# @app.route('/')
+# def home():
+  
+#     return render_template('index.html')
+# @app.route('/', methods=['GET','POST'])
+# def get_restaurants():
+#     #conn = sqlite3.connect('restaurant.db')
+#     c = conn.cursor()
 
-    c.execute(query)
-
-    dishes = c.fetchall()
-    sorted_list = sorted(dishes, key=lambda x: x[1], reverse=True)[:5]
-    # sort(dishes)
-    for i in sorted_list:
-        # Open the image file
-        image = Image.open(i[2])
-        image.show()
- 
-    conn.close()
-    return render_template('dishes.html',top5dishes=sorted_list)
-
-# def top_5drestaurants():
-#     c=conn.cursor()
-   
-
-#     query="SELECT * FROM Restaurants order by rating DESC limit 5"
-   
-#     c.execute(query)
-#     rest=c.fetchall()
+#     query = "select r.r_name,r.r_image_link,d.dish_image_link from restaurants as r join Restaurant_Items as i on i.r_id=r.r_id join Restaurant_Dish as d on d.dish_id=i.dish_id "
     
+#     query += f" where r_name = 'Saffron Spice'"
+    
+
+#     c.execute(query)
+#     restaurants = c.fetchall()
+#     conn.close()
+#     for i in restaurants:
+#         print (i)
+#     return render_template('rest.html',restaurant=restaurants)
+# # with app.app_context():
+# #     get_restaurants('Saffron Spice')
+# @app.route('/dishes', methods=['GET','POST'])
+# def top_5dishes(name,veg = None, cuisine = None ):
+#     c=conn.cursor()
+#     query1= "SELECT r_id from restaurants"
+#     query1 += f" where r_name = '{name}'"
+#     c.execute(query1)
+#     id = c.fetchone()
+#     # print(type(id[0]))
+
+#     query="SELECT d.dish_name, i.rating,d.dish_image_link FROM Restaurant_Dish AS d JOIN Restaurant_Items AS i ON d.dish_id = i.dish_id "
+#     query+=f" where i.r_id='{id[0]}'"
+#     if veg:
+#         query+=f" and d.vegetarian='t'"
+#     if cuisine:
+#         query+=f" and d.cousine ='{cuisine}'"
+
+#     c.execute(query)
+
+#     dishes = c.fetchall()
+#     sorted_list = sorted(dishes, key=lambda x: x[1], reverse=True)[:5]
 #     # sort(dishes)
-#     for i in rest:
+#     for i in sorted_list:
 #         # Open the image file
-#         # image = Image.open(i[4    ])
-#         # image.show()
-#         print(i)
+#         image = Image.open(i[2])
+#         image.show()
  
 #     conn.close()
+#     return render_template('dishes.html',top5dishes=sorted_list)
+
+
+
+
+
 # def restaurants(pin=None,status = None):
 #     c=conn.cursor()
    
@@ -218,21 +215,21 @@ def top_5dishes(name,veg = None, cuisine = None ):
 
 
 # Define a Flask route that inserts a new row into the Customer_cartlist table
-@app.route('/add_to_cartlist')
-def add_to_cartlist():
-    # Retrieve the ID of the currently logged in user from the Flask session
-    customer_id = session.get('customer_id')
+# @app.route('/add_to_cartlist')
+# def add_to_cartlist():
+#     # Retrieve the ID of the currently logged in user from the Flask session
+#     customer_id = session.get('customer_id')
 
-    # Retrieve the item ID and quantity from the request
-    item_id = request.args.get('item_id')
-    quantity = request.args.get('quantity')
+#     # Retrieve the item ID and quantity from the request
+#     item_id = request.args.get('item_id')
+#     quantity = request.args.get('quantity')
 
-    # Insert a new row into the Customer_cartlist table with the retrieved customer ID
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO Customer_cartlist ( item_id, customer_id, number) VALUES ( %s, %s, %s)", (item_id, customer_id, quantity))
-    conn.commit()
+#     # Insert a new row into the Customer_cartlist table with the retrieved customer ID
+#     cursor = conn.cursor()
+#     cursor.execute("INSERT INTO Customer_cartlist ( item_id, customer_id, number) VALUES ( %s, %s, %s)", (item_id, customer_id, quantity))
+#     conn.commit()
 
-    return "Item added to cartlist!"
+#     return "Item added to cartlist!"
 
 
 
